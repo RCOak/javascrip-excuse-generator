696d235c9de3515affd2cbdbe65772f29939c32a
RCOak <coliveirarenato90@gmail.com>
2022-11-02 18:43:36 +0000

    finished

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["the dog ", "my granma ", "his turtle ", "my bird "];
+  let what = ["eat ", "pissed ", "crushed ", "broke "];
+  let when = [
+    "before the class",
+    "right in time",
+    "when i finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let result =
+    who[Math.floor(Math.random() * who.length)] +
+    what[Math.floor(Math.random() * what.length)] +
+    when[Math.floor(Math.random() * when.length)];
+  console.log(result);
+  document.getElementById("excuse").innerHTML = result;
 };
